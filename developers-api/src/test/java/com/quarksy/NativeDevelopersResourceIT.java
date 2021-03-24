dba608b7f7832adaaed5547e86d4e334164bd809
Yixuan Sun <yixsun@deloitte.com.au>
2021-03-24 11:32:54 +1100

    Create project with endpoint /developers exposed

---
+++ developers-api/src/test/java/com/quarksy/NativeDevelopersResourceIT.java
@@ -0,0 +1,9 @@
+package com.quarksy;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeDevelopersResourceIT extends DevelopersResourceTest {
+
+    // Execute the same tests but in native mode.
+}
