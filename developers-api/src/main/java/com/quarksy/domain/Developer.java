8410e98535a4aab8a9099e75fb08d18cdbcc327e
Yixuan Sun <yixsun@deloitte.com.au>
2021-04-14 21:43:44 +1000

    Added lombok

--- developers-api/src/main/java/com/quarksy/domain/Developer.java
+++ developers-api/src/main/java/com/quarksy/domain/Developer.java
@@ -3,10 +3,9 @@ package com.quarksy.domain;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
+@lombok.Data
 @Entity
 public class Developer
 {
@@ -14,49 +13,8 @@ public class Developer
     private UUID id;
     private String name;
     private String team;
-//    private List<String> skills;
+    //    private List<String> skills;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
-
-    public UUID getId() {
-        return id;
-    }
-
-    public void setId(UUID id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getTeam() {
-        return team;
-    }
-
-    public void setTeam(String team) {
-        this.team = team;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
 
 }
