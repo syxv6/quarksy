bde7218541379fbab9e063bbf5c898a88e547403
Yixuan Sun <yixsun@deloitte.com.au>
2021-04-19 23:04:27 +1000

    Restored Lombok

--- developers-api/src/main/java/com/quarksy/domain/Developer.java
+++ developers-api/src/main/java/com/quarksy/domain/Developer.java
@@ -1,63 +1,18 @@
 package com.quarksy.domain;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
-import io.quarkus.hibernate.orm.panache.PanacheEntityBase
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @lombok.Data
 @Entity
-public class Developer extends PanacheEntityBase
-{
-    @Id
-    public String id;
-    public String name;
-    public String team;
-//    public List<String> skills;
-    public LocalDateTime createdAt;
-    public LocalDateTime updatedAt;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getTeam() {
-        return team;
-    }
-
-    public void setTeam(String team) {
-        this.team = team;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
-
+public class Developer extends PanacheEntityBase {
+  @Id
+  public String id;
+  public String name;
+  public String team;
+  //    public List<String> skills;
+  public LocalDateTime createdAt;
+  public LocalDateTime updatedAt;
 }
