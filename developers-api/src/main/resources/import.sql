713d18deafb5db1c8c5237deef5c9083fd5dc527
anicholas <anicholas@deloitte.com.au>
2021-04-26 12:34:29 +1000

    fix: Added dates sql import script

--- developers-api/src/main/resources/import.sql
+++ developers-api/src/main/resources/import.sql
@@ -1,2 +1,2 @@
-INSERT INTO Developer (id, name, team) VALUES ('982f5c0b-33c9-4d17-ba3c-d06c45f28ee1', 'Foo', 'Boo');
-INSERT INTO Developer (id, name, team) VALUES ('982f5c0b-33c9-4d17-ba3c-d06c45f28ee2', 'Foo2', 'Boo2');
+INSERT INTO Developer (id, name, team, createdat, updatedat) VALUES ('982f5c0b-33c9-4d17-ba3c-d06c45f28ee1', 'Foo', 'Boo', '2020-09-20T14:34:23+10:00', '2020-09-20T14:34:23+10:00');
+INSERT INTO Developer (id, name, team, createdat, updatedat) VALUES ('982f5c0b-33c9-4d17-ba3c-d06c45f28ee2', 'Foo2', 'Boo2', '2020-09-20T14:34:23+10:00', '2020-09-20T14:34:23+10:00');
