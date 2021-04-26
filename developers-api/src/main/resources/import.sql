b34ac97651749865febeecba0ac47e48920244c6
anicholas <anicholas@deloitte.com.au>
2021-04-26 15:03:32 +1000

    feature: added skills to import.sql

--- developers-api/src/main/resources/import.sql
+++ developers-api/src/main/resources/import.sql
@@ -1,2 +1,8 @@
 INSERT INTO Developer (id, name, team, createdat, updatedat) VALUES ('982f5c0b-33c9-4d17-ba3c-d06c45f28ee1', 'Foo', 'Boo', '2020-09-20T14:34:23+10:00', '2020-09-20T14:34:23+10:00');
 INSERT INTO Developer (id, name, team, createdat, updatedat) VALUES ('982f5c0b-33c9-4d17-ba3c-d06c45f28ee2', 'Foo2', 'Boo2', '2020-09-20T14:34:23+10:00', '2020-09-20T14:34:23+10:00');
+
+INSERT INTO Skill (id, skill, developer_id) VALUES (1, 'Expert in Quarkus', '982f5c0b-33c9-4d17-ba3c-d06c45f28ee1');
+INSERT INTO Skill (id, skill, developer_id) VALUES (2, 'Expert in Quarkus 2', '982f5c0b-33c9-4d17-ba3c-d06c45f28ee1');
+
+INSERT INTO Skill (id, skill, developer_id) VALUES (3, 'Phd of Quarkus', '982f5c0b-33c9-4d17-ba3c-d06c45f28ee2');
+INSERT INTO Skill (id, skill, developer_id) VALUES (4, 'Phd of Quarkus 2', '982f5c0b-33c9-4d17-ba3c-d06c45f28ee2');
